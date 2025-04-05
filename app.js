10ef65920bd50215496f55d37f88a4213a1f6cf0
tanisha003 <116362718+tanisha003@users.noreply.github.com>
2025-04-05 13:13:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+let userScore=0;
+let compScore=0;
+
+const choices= document.querySelectorAll(".choice")
+const msg=document.querySelector("#msg")
+
+const userScorePara=document.querySelector("#user-score")
+const compScorePara=document.querySelector("#comp-score")
+
+const genCompChoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randIndex=Math.floor(Math.random()*3) //to generate any random number between 0 to 3
+    return options[randIndex]
+
+}
+const drawGame=()=>{
+    msg.innerText="Game was Draw. Play Again/"
+    msg.style.backgroundColor="rgb(3, 55, 101)"
+}
+
+const showWinner=(userWin, userChoice,compChoice)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText=userScore
+        msg.innerText=`You Win! Your ${userChoice} beats ${compChoice}`
+        msg.style.backgroundColor="green"
+    }
+    else{
+        compScore++;
+        compScorePara.innerText=compScore
+        msg.innerText=`You lost :( ${compChoice} beats your ${userChoice}`
+        msg.style.backgroundColor="red"
+    }
+}
+
+const playGame=(userChoice)=>{
+    //generate computer choice
+    const compChoice=genCompChoice();
+    console.log("compchoice=",compChoice)
+    if(userChoice===compChoice){
+        drawGame();
+    }
+    else{
+        let userWin=true;
+        if(userChoice==="rock"){
+            //scissors,paper
+            userWin=compChoice==="paper"?false:true;
+        }
+        else if(userChoice==="paper"){
+            //rock, scissors
+            userWin=compChoice==="sciccors"?false:true;
+        }
+        else{
+            //rock,paper
+            userWin=compChoice==="rock"?false:true;
+        }
+        showWinner(userWin,userChoice,compChoice);
+    }
+};
+
+choices.forEach((choice)=>{
+    const userChoice=choice.getAttribute("id")
+    choice.addEventListener("click",()=>{
+    console.log("choice was clicked",userChoice)
+    playGame(userChoice)
+    });
+});
